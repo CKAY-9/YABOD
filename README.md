9349dee805aec7293625da885d215b26d560b76e
Cameron <foxtrotcyanide@gmail.com>
2023-07-06 14:51:20 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Yet Another Ban on Death Plugin
+
+This is a plugin I made for personal use, so it doesn't really provide all that much.
+All this does is ban a player for a certain amount of time (in seconds) when they die.
---
+++ src/main/java/dev/yabod/YABOD.java
@@ -0,0 +1,22 @@
+package dev.yabod;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class YABOD extends JavaPlugin {
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent e) {
+        
+    }
+    
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
